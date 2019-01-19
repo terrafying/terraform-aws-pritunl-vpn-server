79852d6cb9cad7c7e0ebf3d81232a834a8c2213e
terrafying <38737064+terrafying@users.noreply.github.com>
2019-01-18 21:20:23 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ data "template_file" "user_data" {
   template = "${file("${path.module}/templates/user_data.sh.tpl")}"
 
   vars {
-    aws_region          = "${var.aws_region.current.name}"
+    aws_region          = "${var.aws_region}"
     s3_backup_bucket    = "${local.backup_bucket_name}"
     healthchecks_io_key = "/pritunl/${var.resource_name_prefix}/healthchecks-io-key"
   }
