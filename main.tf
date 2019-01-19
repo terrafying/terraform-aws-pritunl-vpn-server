1f378de92b4db40efd1a3490d226e575e6f8a8a9
terrafying <38737064+terrafying@users.noreply.github.com>
2019-01-18 21:21:47 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ data "template_file" "iam_instance_role_policy" {
   vars {
     s3_backup_bucket     = "${local.backup_bucket_name}"
     resource_name_prefix = "${var.resource_name_prefix}"
-    aws_region           = "${data.aws_region.current.name}"
+    aws_region           = "${var.aws_region}"
     account_id           = "${data.aws_caller_identity.current.account_id}"
     ssm_key_prefix       = "/pritunl/${var.resource_name_prefix}/*"
   }
