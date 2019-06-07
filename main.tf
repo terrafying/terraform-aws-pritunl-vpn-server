9b1a37e414756ce0400e688e38f0d777e3ed9eaa
terrafying <38737064+terrafying@users.noreply.github.com>
2019-06-07 18:02:45 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -294,6 +294,6 @@ resource "aws_instance" "pritunl" {
 }
 
 resource "aws_eip" "pritunl" {
-  instance = "${aws_instance.pritunl.*.id}"
+  instance = "${aws_instance.pritunl[0].id}"
   vpc      = true
 }
